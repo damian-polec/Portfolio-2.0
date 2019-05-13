fd1200fa4ec316ad7cb956b2aa16da4af4361fc6
Damian Polec <damian.polec11@gmail.com>
2019-05-13 17:45:22 +0200

    Delete facebook link

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -6,9 +6,8 @@ import './Socials.scss';
 const socials = (props) => {
   return (
     <div className={props.view}>
-        <a href='https://www.facebook.com/damien.polec' target="_blank" rel="noopener noreferrer"><Icon iconType={['fab', 'facebook-f']} /></a>
         <a href="https://github.com/damian-polec" target="_blank" rel="noopener noreferrer"><Icon iconType={['fab', 'github']} /></a>
-        <a href="https://www.linkedin.com/in/damian-po%C5%82e%C4%87-46167912b/" target="_blank" rel="noopener noreferrer"><Icon iconType={['fab', 'linkedin-in']} /></a>
+        <a href="www.linkedin.com/in/damian-polec" target="_blank" rel="noopener noreferrer"><Icon iconType={['fab', 'linkedin-in']} /></a>
     </div>
   )
 }
